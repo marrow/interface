93729a62ffd4710557f2f04cdde682afb02b125f
Alice Bevan-McGregor <alice@gothcandy.com>
2011-08-19 19:13:06 -0400

    Unification of release information, setup script, and setup configuration.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import sys
 import os
+import sys
 
 from setuptools import setup, find_packages
 
@@ -10,45 +10,55 @@ from setuptools import setup, find_packages
 if sys.version_info < (2, 6):
     raise SystemExit("Python 2.6 or later is required.")
 
-exec(open(os.path.join("marrow", "interface", "release.py")).read())
+exec(open(os.path.join("marrow", "interface", "release.py")))
+
 
 
 setup(
-    name="marrow.interface",
-    version=version,
-    
-    description="An anti-Pythonic declarative strict interface definition and validation system.",
-    author="Alice Bevan-McGregor",
-    author_email="alice+marrow@gothcandy.com",
-    url="https://github.com/marrow/marrow.interface",
-    download_url="http://pypi.python.org/pypi/marrow.interface",
-    license="MIT",
-    keywords="",
-    
-    test_suite="nose.collector",
-    tests_require=["nose", "coverage"],
-    
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.1",
-        "Programming Language :: Python :: 3.2",
-        "Topic :: Software Development :: Libraries :: Python Modules"
-    ],
-    
-    packages=find_packages(exclude=["examples", "tests"]),
-    zip_safe=True,
-    include_package_data=True,
-    package_data={
-        "": ["README.textile", "LICENSE", "distribute_setup.py"]
-    },
-    
-    namespace_packages=["marrow"]
-)
+        name = "marrow.interface",
+        version = version,
+        
+        description = "An anti-Pythonic declarative strict interface definition and validation system.",
+        long_description = """\
+For full documentation, see the README.textile file present in the package,
+or view it online on the GitHub project page:
+
+https://github.com/marrow/marrow.interface""",
+        
+        author = "Alice Bevan-McGregor",
+        author_email = "alice+marrow@gothcandy.com",
+        url = "https://github.com/marrow/marrow.interface",
+        license = "MIT",
+        
+        install_requires = [
+            'marrow.util < 2.0'
+        ],
+        
+        test_suite = 'nose.collector',
+        tests_require = [
+            'nose',
+            'coverage'
+        ],
+        
+        classifiers = [
+            "Development Status :: 5 - Production/Stable",
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.6",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.1",
+            "Programming Language :: Python :: 3.2",
+            "Topic :: Software Development :: Libraries :: Python Modules"
+        ],
+        
+        packages = find_packages(exclude=['examples', 'tests']),
+        zip_safe = True,
+        include_package_data = True,
+        package_data = {'': ['README.textile', 'LICENSE']},
+        
+        namespace_packages = ['marrow'],
+    )
