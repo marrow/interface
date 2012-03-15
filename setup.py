ae25e2efa7e3968d53584477e38536d8e6209a61
Nando Florestan <nandoflorestan@gmail.com>
2012-03-15 02:39:38 -0300

    Don't crash under Python 3 which has the new __qualname__
    Also fixed a problem in setup.py; couldn't *setup.py develop*
    under Python 3.3 alpha 1.

--- setup.py
+++ setup.py
@@ -10,36 +10,36 @@ from setuptools import setup, find_packages
 if sys.version_info < (2, 6):
     raise SystemExit("Python 2.6 or later is required.")
 
-exec(open(os.path.join("marrow", "interface", "release.py")))
+exec(open(os.path.join("marrow", "interface", "release.py")).read())
 
 
 
 setup(
         name = "marrow.interface",
         version = version,
-        
+
         description = "An anti-Pythonic declarative strict interface definition and validation system.",
         long_description = """\
 For full documentation, see the README.textile file present in the package,
 or view it online on the GitHub project page:
 
 https://github.com/marrow/marrow.interface""",
-        
+
         author = "Alice Bevan-McGregor",
         author_email = "alice+marrow@gothcandy.com",
         url = "https://github.com/marrow/marrow.interface",
         license = "MIT",
-        
+
         install_requires = [
             'marrow.util < 2.0'
         ],
-        
+
         test_suite = 'nose.collector',
         tests_require = [
             'nose',
             'coverage'
         ],
-        
+
         classifiers = [
             "Development Status :: 5 - Production/Stable",
             "Environment :: Console",
@@ -54,11 +54,11 @@ https://github.com/marrow/marrow.interface""",
             "Programming Language :: Python :: 3.2",
             "Topic :: Software Development :: Libraries :: Python Modules"
         ],
-        
+
         packages = find_packages(exclude=['examples', 'tests']),
         zip_safe = True,
         include_package_data = True,
         package_data = {'': ['README.textile', 'LICENSE']},
-        
+
         namespace_packages = ['marrow'],
     )
