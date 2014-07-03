04b8852fc4d749c3677050174c18a1231c59f5cb
Alice Bevan–McGregor <alice@gothcandy.com>
2014-07-03 03:14:39 -0400

    Updated release information to follow convention.

--- marrow/interface/release.py
+++ marrow/interface/release.py
@@ -5,9 +5,10 @@
 from collections import namedtuple
 
 
-__all__ = ['version_info', 'version']
-
-
-version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(1, 0, 0, 'final', 0)
-
+version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(1, 0, 1, 'final', 0)
 version = ".".join([str(i) for i in version_info[:3]]) + ((version_info.releaselevel[0] + str(version_info.serial)) if version_info.releaselevel != 'final' else '')
+
+author = namedtuple('Author', ['name', 'email'])("Alice Bevan-McGregor", 'alice@gothcandy.com')
+
+description = "An anti-Pythonic declarative strict interface definition and validation system."
+url = 'https://github.com/marrow/marrow.interface/'
