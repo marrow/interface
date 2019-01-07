fb0644c63476a6cb9783c307c10c06b41a80ecbf
Alice Bevan-McGregor <alice@gothcandy.com>
2019-01-07 09:34:09 -0500

    Python 3 namespace packaging update, metadata updates.

--- setup.py
+++ setup.py
@@ -1,37 +1,18 @@
-#!/usr/bin/env python
-# encoding: utf-8
+#!/usr/bin/env python3
 
 import os
 import sys
 import codecs
 
-try:
-	from setuptools.core import setup, find_packages
-except ImportError:
-	from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
 from setuptools.command.test import test as TestCommand
 
 
-if sys.version_info < (2, 6):
-	raise SystemExit("Python 2.6 or later is required.")
-elif sys.version_info > (3, 0) and sys.version_info < (3, 2):
-	raise SystemExit("Python 3.2 or later is required.")
+if sys.version_info < (3, 3):
+	raise SystemExit("Python 3.3 or later is required.")
 
 exec(open(os.path.join("marrow", "interface", "release.py")).read())
-
-
-class PyTest(TestCommand):
-	def finalize_options(self):
-		TestCommand.finalize_options(self)
-
-		self.test_args = []
-		self.test_suite = True
-
-	def run_tests(self):
-		import pytest
-		sys.exit(pytest.main(self.test_args))
-
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -49,7 +30,7 @@ setup(
 	author_email = author.email,
 	
 	license = 'MIT',
-	keywords = '',
+	keywords = ('interface enforcement',),
 	classifiers = [
 			"Development Status :: 5 - Production/Stable",
 			"Environment :: Console",
@@ -57,19 +38,25 @@ setup(
 			"License :: OSI Approved :: MIT License",
 			"Operating System :: OS Independent",
 			"Programming Language :: Python",
-			"Programming Language :: Python :: 2.7",
 			"Programming Language :: Python :: 3",
-			"Programming Language :: Python :: 3.2",
 			"Programming Language :: Python :: 3.3",
 			"Programming Language :: Python :: 3.4",
+			"Programming Language :: Python :: 3.5",
+			"Programming Language :: Python :: 3.6",
+			"Programming Language :: Python :: 3.7",
 			"Programming Language :: Python :: Implementation :: CPython",
 			"Programming Language :: Python :: Implementation :: PyPy",
 			"Topic :: Software Development :: Libraries :: Python Modules"
 		],
 	
-	packages = find_packages(exclude=['test', 'script', 'example']),
+	packages = ('marrow.interface', ),
 	include_package_data = True,
-	namespace_packages = ['marrow'],
+	package_data = {'': ['README.rst', 'LICENSE.txt']},
+	zip_safe = False,
+	
+	setup_requires = [
+			'pytest-runner',
+		] if {'pytest', 'test', 'ptr'}.intersection(sys.argv) else [],
 	
 	install_requires=[
 			'marrow.schema',
@@ -80,9 +67,4 @@ setup(
 		),
 	
 	tests_require = tests_require,
-	
-	zip_safe = False,
-	cmdclass = dict(
-			test = PyTest,
-		)
 )
